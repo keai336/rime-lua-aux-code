7104670d37845644ac11e65f44b6f3bd38da95f3
like <keai336wxy@gmail.com>
2025-04-09 14:52:50 +0800

    修正记忆行为，只记忆词

--- lua/aux_code_for_win.lua
+++ lua/aux_code_for_win.lua
@@ -227,22 +227,17 @@ function AuxFilter.main1_notifier(ctx)
         else
             -- 剩下的直接上屏
             -- log.info(AuxFilter.transdcode,AuxFilter.removeAuxInput)
-            ctx:commit()
-            ctx.commit_history:pop_back()
-            local commit = ctx.commit_history:back()
             -- logdic(commit.text .. "|" .. commit.type .. "|" .. AuxFilter.removeAuxInput .. "|" .. AuxFilter.transdcode .. "|" .. xh_sp_code_2_qp(AuxFilter.removeAuxInput))
             --- 只记录词
-            local entry = DictEntry()
-            entry.text = AuxFilter.transdcode:gsub("‸+$", "")
-            -- logdic(entry.text)
-            entry.custom_code = xh_sp_code_2_qp(AuxFilter.removeAuxInput) .. " "
-            AuxFilter.memory:start_session()
-            local r = AuxFilter.memory:update_userdict(entry, 1, "")
-            AuxFilter.memory:finish_session()
-            -- if utf8len(AuxFilter.transdcode)>1 then
-            --     -- logdic(AuxFilter.transdcode .. "," .. AuxFilter.removeAuxInput) 
-  
-            -- end
+            if utf8len(AuxFilter.transdcode)>1 then
+                local entry = DictEntry()
+                entry.text = AuxFilter.transdcode:gsub("‸+$", "")
+                -- logdic(entry.text)
+                entry.custom_code = xh_sp_code_2_qp(AuxFilter.removeAuxInput) .. " "
+                AuxFilter.memory:start_session()
+                local r = AuxFilter.memory:update_userdict(entry, 1, "")
+                AuxFilter.memory:finish_session()
+            end
 
         end
     end
