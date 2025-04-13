84f51b6e91acaf0b9084f997e6d81bb19b05ae75
like <keai336wxy@gmail.com>
2025-04-13 22:58:15 +0800

    修补了动态响应的逻辑；修正了获取默认长度的逻辑

--- lua/aux_code_for_win.lua
+++ lua/aux_code_for_win.lua
@@ -494,7 +494,8 @@ end
 function AuxFilter.yield_candisub(cand)
     if AuxFilter.counter==0 then
         if AuxFilter.ficompensate == nil then
-            AuxFilter.ficompensate = (cand._end-cand._start)/2
+            -- AuxFilter.ficompensate = (cand._end-cand._start)/2
+            AuxFilter.ficompensate = utf8len(cand.text)
         end
         AuxFilter.ficompensate = AuxFilter.ficompensate - AuxFilter.leftcompen + AuxFilter.rightcompen
         if AuxFilter.ficompensate<=0 then
@@ -520,7 +521,7 @@ function AuxFilter.yield_candisub(cand)
     AuxFilter.yieldset[finalcandi.text] = true
     if AuxFilter.skipc<=0 then
         AuxFilter.counter = AuxFilter.counter+1
-        if AuxFilter.counter == 1 then
+        if AuxFilter.counter == 1  and AuxFilter.dynimic_or then
             AuxFilter.last_fist_commit = finalcandi.text
         end
         yield(finalcandi)
@@ -987,6 +988,7 @@ function AuxFilter.func(input, env)
     local pattern_long = "^%a+" ..AuxFilter.trigger_key_pattern .. "%a*" .. AuxFilter.trigger_key_pattern .."+%a*$" --长句修改分支的正则
     local pattern_singlechar = "^" .. AuxFilter.trigger_key_pattern .. "%a%a%a?%a?$"  -- 单字输入分支
     local pattern_odds_char = ""
+    AuxFilter.dynimic_or = true
     if string.match(AuxFilter.inputCode,pattern_main1)then
         -- log.info("进入分支1",AuxFilter.inputCode)
         AuxFilter.main1(input,env)
@@ -998,6 +1000,8 @@ function AuxFilter.func(input, env)
         -- log.info("进入分支3",AuxFilter.inputCode)
     --都不匹配直接返回的分支
     else
+        AuxFilter.dynimic_or = false
+        AuxFilter.last_fist_commit = nil
         AuxFilter.defaultmain(input,env)
     end
     -- local url = "https://chatgpt.com/c/67f3a89e-d250-8009-8373-ddd2e5b7eb6f"
